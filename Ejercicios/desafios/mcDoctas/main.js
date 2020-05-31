1793db3fb8b5c92fa62c4f5d51c7e407d7b4ba70
pilichanampe <pilichanampe@hotmail.com>
2020-05-31 18:55:08 -0300

    Trying to fix some bugs

--- Ejercicios/desafios/mcDoctas/main.js
+++ Ejercicios/desafios/mcDoctas/main.js
@@ -1,61 +1,105 @@
-/*function increaseValue() {
-  var value = parseInt(document.getElementById('number').value, 10);
-  value = isNaN(value) ? 0 : value;
-  value++;
-  document.getElementById('number').value = value;
-}
-
-function decreaseValue() {
-  var value = parseInt(document.getElementById('number').value, 10);
-  value = isNaN(value) ? 0 : value;
-  value < 1 ? value = 1 : '';
-  value--;
-  document.getElementById('number').value = value;
-  console.log(document.getElementById('number').value);
-}*/
-
+// Calcula el total del pedido. Esta función se llama cuando la persona
+// modifica la cantidad de hamburguesas, los toppings o los aderezos.
+// Como resultado, muestra en pantalla el total del pedido.
 function calcularTotal() {
-  const preciotomate = 15;
-  const preciohuevo = 40;
-  const precioqueso = 20;
-  const preciomayonesa = 10;
-  const preciomostaza = 10;
-  const precioketchup = 10;
-  const preciohamburguesa = 200;
+  const preciosProductos = {
+    tomate: 15,
+    huevo: 40,
+    queso: 20,
+    mayonesa: 10,
+    mostaza: 10,
+    ketchup: 10,
+    hamburguesa: 200,
+  };
+  
   const tomate = document.querySelector(".tomate").value;
   const huevo = document.querySelector(".huevo").value;
   const queso = document.querySelector(".queso").value;
-  const hamburguesas = document.querySelector(".hamburguesas").value;
+  const hamburguesas = document.querySelector("[data-hamburguesas]").value;
   const mayonesa = document.querySelector(".mayonesa").checked;
   const ketchup = document.querySelector(".ketchup").checked;
   const mostaza = document.querySelector(".mostaza").checked;
 
-  const totalTomate = preciotomate * tomate;
-  const totalHuevo = preciohuevo * huevo;
-  const totalQueso = precioqueso * queso;
-  const totalHamburguesa = preciohamburguesa;
+  const totalTomate = preciosProductos.tomate * tomate;
+  const totalHuevo = preciosProductos.huevo * huevo;
+  const totalQueso = preciosProductos.queso * queso;
+  const totalHamburguesa = preciosProductos.hamburguesa;
    
   let total = totalHamburguesa + totalTomate + totalHuevo + totalQueso;
   
   if (mayonesa) { 
-    total += preciomayonesa;
+    total += preciosProductos.mayonesa;
   }
   
   if (mostaza) {
-    total += preciomostaza;
+    total += preciosProductos.mostaza;
   }
   
   if (ketchup) { 
-    total += precioketchup;
+    total += preciosProductos.ketchup;
   }
 
   const totalPedido = document.querySelector(".totalPedido");
   totalPedido.value = total * hamburguesas;
 }
 
+// Incrementa en 1 la cantidad de hamburguesas del pedido actual
+// y muestra la nueva cantidad en pantalla. Esta función se llama
+// cuando la persona hace click sobre el botón "+".
+function agregarHamburguesa() {
+  cantidadHamburguesas += 1;
+  actualizarConteoHamburguesas();
+  calcularTotal();
+}
+
+// Incrementa en 1 la cantidad de hamburguesas del pedido actual
+// y muestra la nueva cantidad en pantalla. Esta función se llama
+// cuando la persona hace click sobre el botón "+".
+function quitarHamburguesa() {
+  if (cantidadHamburguesas <= 1) {
+    return;
+  }
+  
+  cantidadHamburguesas -= 1;
+  actualizarConteoHamburguesas();  
+  calcularTotal();
+}
+
+// Muestra en pantalla la cantidad de hamburguesas del pedido.
+// Esta función se llama luego de cambiar la cantidad de hamburguesas
+// del pedido.
+function actualizarConteoHamburguesas() {
+  // Accedo al campo numérico "Hamburguesas".
+  const campoHamburguesas = document.querySelector("[data-hamburguesas]");
+  
+  // Asigno el valor de "cantidadHamburguesas" al campo
+  // para que se vea en pantalla.
+  campoHamburguesas.value = cantidadHamburguesas;  
+}
+
+// ////////////////////////////////////////////////////////////////////////////
+// Todo este código se ejecuta apenas se carga la página, de aquí para abajo
+// ////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////
+// INICIALIZACIÓN DE LA PANTALLA
+
+let cantidadHamburguesas = 1;
+
+// Muestro en pantalla la cantidad inicial de hamburguesas del pedido (1).
+actualizarConteoHamburguesas();
+
 // Hago un cálculo del total con el estado inicial de la aplicación
 // (esto es, por defecto 1 hamburguesa sin toppings ni aderezos.)
 calcularTotal();
+
+//
+///////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////
+// CONFIGURO EVENTO PARA CALCULAR TOTAL
+// Este evento ocurre cuando la persona cambia algún topping o aderezo o
+// la cantidad de hamburguesas de su pedido.
 
 // Selecciono todos los campos que afecten al cálculo del total
 // (en HTML, están marcados con el atributo "data-afectatotal").
@@ -67,9 +111,59 @@ for (let campo = 0; campo < camposAfectaTotal.length; campo += 1) {
   // que el usuario haga un cambio en su selección.
   camposAfectaTotal[campo].addEventListener("change", calcularTotal);
 }
+//
+///////////////////////////////////////////////////////////////////////////////////
 
-// Agrego la clase "oculto" del elemento.
-document.querySelector("body").classList.add("oculto");
+///////////////////////////////////////////////////////////////////////////////////
+// CONFIGURO EVENTO PARA AGREGAR Y QUITAR HAMBURGUESAS
+// Este evento ocurre cuando la persona cambia la cantidad de hamburguesas de su pedido.
 
-// Quito la clase "oculto" del elemento.
-document.querySelector("body").classList.remove("oculto");
+const botonAgregarHamburguesa = document.querySelector(".inc");
+const botonQuitarHamburguesa = document.querySelector(".dec");
+
+botonAgregarHamburguesa.addEventListener("click", agregarHamburguesa);
+botonQuitarHamburguesa.addEventListener("click", quitarHamburguesa);
+
+///////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////
+// CONFIGURO EVENTO PARA IR AL SIGUIENTE PASO.
+// Este evento ocurre cuando la persona hace click en el botón "Siguiente".
+
+function ocultarSeccion(expresionCSS) {
+  // Ocultamos una sección de la pantalla.
+  const seccion = document.querySelector(expresionCSS);
+  seccion.style.display = "none";
+}
+
+function mostrarSeccion(expresionCSS) {
+  // Mostramos una sección de la pantalla.
+  const seccion = document.querySelector(expresionCSS);
+  seccion.style.display = "flex";
+}
+
+const botonPasoADatosCliente = document.querySelector(".pedido-container .boton-siguiente");
+
+botonPasoADatosCliente.addEventListener("click", function () {
+  ocultarSeccion(".pedido-container");
+  mostrarSeccion(".datos-cliente");
+});
+
+const botonPasoADatosPago = document.querySelector(".datos-cliente .boton-siguiente");
+
+botonPasoADatosPago.addEventListener("click", function () {
+  ocultarSeccion(".datos-cliente");
+  mostrarSeccion(".pago-select");
+});
+
+//
+///////////////////////////////////////////////////////////////////////////////////
+
+// Agregar botón "Atrás".
+
+// Limpiar primera pantalla.
+
+// Guardar selección de forma de pago.
+
+// Al clickear en siguiente en "pago-select", mostrar la pantalla
+// correspondiente según la forma de pago elegida.
