92b9c14619d03ce3d3117a2aac21560d45a4ca2d
pilichanampe <pilichanampe@hotmail.com>
2020-06-11 12:59:55 -0300

    watch-date functioning

--- Ejercicios/desafios/watchTracker/app/watch-date/watch-date.js
+++ Ejercicios/desafios/watchTracker/app/watch-date/watch-date.js
@@ -1,6 +1,37 @@
 class WatchDate extends HTMLElement {
   constructor() {
     super();
-    cargarTemplate("#watch-date", this);
+    loadTemplate("#watch-date", this);
+  }
+
+  connectedCallback() {
+    this.loadDate();
+  }
+
+  loadDate() {
+    const date = new Date();
+    const day = date.getDay();
+    const dateDay = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+
+    const days = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+    const nameDay = days[day].slice(0, 3).toUpperCase();
+    console.log(nameDay);
+
+    const months = ["Enero", "Febrero", "Marzo", "Abril" ,"Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    const nameMonth = months[month].slice(0, 3).toUpperCase();
+
+
+    //Actualizo el Shadow DOM
+    this.shadowRoot.querySelector('p').innerHTML = nameDay + " " + dateDay + " " + nameMonth + " " + year;
+  }
+
+  updateDate() {
+    setInterval(() => {      
+      this.loadDate();    
+    }, 1000);  
   }
 }
+
+window.customElements.define("watch-date", WatchDate);
