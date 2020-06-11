a3109b0da8d44be16133a7d6708728129bb010a6
pilichanampe <pilichanampe@hotmail.com>
2020-06-10 22:46:37 -0300

    updating watch-tracker template

--- Ejercicios/desafios/watchTracker/app/watch-tracker/watch-tracker.js
+++ Ejercicios/desafios/watchTracker/app/watch-tracker/watch-tracker.js
@@ -3,6 +3,31 @@ class WatchTracker extends HTMLElement {
       super();
       loadTemplate("#watch-tracker", this);
     }
+
+    connectedCallback() {      
+      this.updateTracker(100);
+    }
+
+    updateHeartbeat(frequency) {
+      const beatInterval = 60/frequency;
+      
+      this.shadowRoot.querySelector("#heart").style.animation = `blink ${beatInterval.toString()}s infinite`;
+      return beatInterval;
+    }
+
+    updateCalories() {
+
+    }
+
+    updateTracker(frequency) {
+      this.shadowRoot.querySelector("#bpm").innerHTML = `${frequency}`;
+      this.updateHeartbeat(frequency);
+    }
+
+   
+
+
 }
 
 window.customElements.define("watch-tracker", WatchTracker);
+
